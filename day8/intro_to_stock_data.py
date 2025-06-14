518370de0cc5a40480a1f55f214baad803e9903e
Ayotunde Akinboade <akinboadelawrenceayo@gmail.com>
2025-06-14 17:30:39 +0100

    update day8

--- day8/intro_to_stock_data.py
+++ day8/intro_to_stock_data.py
@@ -2,11 +2,16 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
+# get stock data from yahoofinance
 df = yf.download("NVDA", start="2024-01-01", end="2024-12-01")
 
+# get the first 5 rows
 print(df.head())
+# get the information on each column
 print(df.info())
+# get the statistical information of each column
 print(df.describe())
 
-df["Close"].plot(title="Apple Stock Closing price")
+# Plot the closing price of the stock data
+df["Close"].plot(title="NVIDIA Stock Closing price")
 plt.show()
